44556220a13f87ce887d9efb4fb7709c9fc74b21
Josh Jancula <josh@jancula.com>
2018-05-09 04:48:19 +0000

    response

--- public/js/products.js
+++ public/js/products.js
@@ -1,16 +1,17 @@
 $(document).ready(function() {
-    
+    $("#typeView").hide();
     $("#marketView").hide();
-   
+     $(".productImageMain").hide();
+
     var products = [
         {
-            name: "petrolium",
+            name: "Petrolium",
             button: "petGal",
             info: "McGee Corporation has been a national supplier of petroleum canopies for over 40 years to various retail, commercial, and governmental agencies requiring canopy structures for their fueling operations. Our list of customers and markets include but are not limited to the following well known brands:<br>Major Oil Companies: Exxon/Mobil, Shell, Chevron/Texaco, BP, Marathon<br>Convenience Store chains: 7-Eleven, Circle K, Sheetz, The Pantry<br>ruck Stops/Travel Plazas: Marathon, Wilco Hess<br>Hypermarket, Grocery & Retail stores: Sam's Club, Kroger, Ingles<br>Car Rental/Fleet Fueling: Enterprise<br>Commercial/Governmental Agencies: fueling sites/toll operations<br><br>Our canopy systems are designed to provide superior branding image, identification, and functionality that enhance our customer's fueling facilities and image standards. Engineering designs meet or exceed local zoning and architectural requirements that are site specific to insure compliance with all governmental codes and regulations.<br><br>As the premier provider of fueling services for your retail, commercial, or governmental locations, McGee Corporation's Commitment to Excellence is sure to deliver a quality, engineered petroleum canopy designed to meet all your brand and image needs."
             
         },
         {
-            name: "banking",
+            name: "Banking",
             button: "bankGal",
             info: "McGee Corporation knows that financial facilities must have an architectural image that attracts customers. With over 40 years of canopy experience, our company creates dynamic, highly-visible drive-thru canopy systems that enhance your financial institution's products and services. Our design teams create classical architectural canopy systems which expressed integrity, endurance and confidence.<br><br>As a bank or financial institution, McGee Corporation's 'Commitment to Excellence' will provide you with drive-thru canopy systems are designed to provide superior market image, identification, and functionality that delivers your brand message day or night"
         },
@@ -33,9 +34,9 @@ $(document).ready(function() {
             $("#productInfo").empty();
         var div = $("<div class='card'>").append(
             
-            "<div class='card-stacked'>" + "<div class='card-content'>" +
+            "<div class='card dynamicCard'>" + "<div class='card-content'>" +
           "<h4>" + product.name + "</h4>" +
-          "<p style='font-size: 10px;'>" + product.info + "</p>" +
+          "<p style='font-size: 12px;'>" + product.info + "</p><br>" +
           "<button data-target='viewGallery' class='btn modal-trigger view red darken-2'  data-id='" + product.button+ "'>View Gallery</button>" +
           "</div>" 
           );
@@ -47,26 +48,81 @@ $(document).ready(function() {
     
     
      $("#petroleum").on("click", function(event) {
-         document.getElementById('displayCard').style.margin-left = 0.ToString() + '%';
+        document.getElementById("productCard").style.marginRight = "0%";
+        document.getElementById("displayCard").style.marginLeft = "0%";
         event.preventDefault();
         getInfo(products[0]);
     });
     
     $("#petroleum2").on("click", function(event) {
+        document.getElementById("productCard").style.marginRight = "0%";
+        document.getElementById("displayCard").style.marginLeft = "0%";
         event.preventDefault();
         getInfo(products[0]);
     });
 
     $("#banking").on("click", function(event) {
+        document.getElementById("productCard").style.marginRight = "0%";
+        document.getElementById("displayCard").style.marginLeft = "0%";
         event.preventDefault();
         getInfo(products[1]);
     });
     
     $("#banking2").on("click", function(event) {
+        document.getElementById("productCard").style.marginRight = "0%";
+        document.getElementById("displayCard").style.marginLeft = "0%";
         event.preventDefault();
         getInfo(products[1]);
     });
-
     
+    $("#misc").on("click", function(event) {
+        document.getElementById("productCard").style.marginRight = "0%";
+        document.getElementById("displayCard").style.marginLeft = "0%";
+        event.preventDefault();
+        getInfo(products[2]);
+    });
+    
+    $("#misc2").on("click", function(event) {
+        document.getElementById("productCard").style.marginRight = "0%";
+        document.getElementById("displayCard").style.marginLeft = "0%";
+        event.preventDefault();
+        getInfo(products[2]);
+    });
+    
+    $("#retail").on("click", function(event) {
+        document.getElementById("productCard").style.marginRight = "0%";
+        document.getElementById("displayCard").style.marginLeft = "0%";
+        event.preventDefault();
+        getInfo(products[2]);
+    });
+    
+    $("#retail2").on("click", function(event) {
+        document.getElementById("productCard").style.marginRight = "0%";
+        document.getElementById("displayCard").style.marginLeft = "0%";
+        event.preventDefault();
+        getInfo(products[2]);
+    });
+
+    $("#market").on("click", function(event) {
+        document.getElementById("productCard").style.marginRight = "0%";
+        document.getElementById("displayCard").style.marginLeft = "0%";
+        event.preventDefault();
+        getInfo(products[0]);
+       $("#typeView").hide();
+       $("#marketView").show();
+        $(".productImageMain").show();
+   
+    });
+    
+     $("#type").on("click", function(event) {
+        document.getElementById("productCard").style.marginRight = "0%";
+        document.getElementById("displayCard").style.marginLeft = "0%";
+        event.preventDefault();
+        getInfo(products[0]);
+       $("#marketView").hide();
+       $("#typeView").show();
+        $(".productImageMain").show();
+   
+    });
     
 })
